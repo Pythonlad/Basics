e04e959e8c6d49bcf7b646bd33b689206b41adce
Patrick Riungu <55322755+Pytriq@users.noreply.github.com>
2019-09-18 11:01:31 +0300

    Create while right.py

---
+++ while right.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 10:56:56 2019
+
+@author: Patrick
+"""
+
+n = input("Go right or left: ")
+while n == "right":
+    n = input("keep going right or left to exit: ")
+print("You are out!!")
