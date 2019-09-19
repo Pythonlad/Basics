014ddc7ad23cd9b88f58f75486ebc650b5d96b10
Patrick Riungu <55322755+Pytriq@users.noreply.github.com>
2019-09-19 12:26:06 +0300

    Create squareiterations.py

---
+++ squareiterations.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 11:24:42 2019
+
+@author: Patrick
+"""
+
+#iteration to find the square
+
+squareOf = 100
+iterations = squareOf
+square = 0
+while iterations != 0:
+    square+=squareOf
+    iterations-=1
+print(square)
+    
