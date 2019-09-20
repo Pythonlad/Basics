b0b5a806c91a4ce021fa3a89572d3937cdff4c6f
Patrick Riungu <55322755+Pytriq@users.noreply.github.com>
2019-09-20 18:51:41 +0300

    Create numOfVowels.py

---
+++ numOfVowels.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 18:22:17 2019
+
+@author: Patrick
+"""
+#count the number of vowels in a string
+s = "bob where bobeverbob sleeplast nibobob, bob I don't boby"
+
+print("Number of vowels:",
+      (s.count('a')+s.count('e')+s.count('i')
+     +s.count('o')+s.count('u')))
